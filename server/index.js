73a89e7e78bd7d8b7180a8ac9d96c78be6a302e1
José Alisson <josealissonif@gmail.com>
2023-09-20 00:31:38 -0300

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import cors from 'cors'
+import express from 'express'
+import { download } from './download.js'
+import { transcribe } from './transcribe.js'
+import { summarize } from './summarize.js'
+import { convert } from './converter.js'
+const app = express()
+app.use(express.json())
+app.use(cors())
+const port = 3333
+
+app.get('/summary/:id', async (request, response) => {
+  try {
+    await download(request.params.id);
+    const audioConverted = await convert()
+
+    const result = await transcribe(audioConverted)
+    return response.json({ result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
+})
+
+app.post('/summary', async (request, response) => {
+  try {
+    const result = await summarize(request.body.text)
+    return response.json({ result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
